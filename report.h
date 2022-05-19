b62a6cb64e9e35951c32b7fa71102b611dfae506
WOOHI84 <97064107+IT21169830@users.noreply.github.com>
2022-05-19 17:51:07 +0530

    Create report.h

---
+++ report.h
@@ -0,0 +1,13 @@
+class Report
+{
+	private:
+		int ReportID;
+		int propertyID;
+		char propertyType;
+		
+	public:
+		Report();
+		Report(int rpid ,int PropertyID , char PropertyType);
+		void displayReport();
+    ~Report();
+};
